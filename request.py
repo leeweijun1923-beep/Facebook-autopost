775713a6b0c334a5f7fe9e865a6004a8dbcc6f17
leeweijun1923-beep <leeweijun1923@gmail.com>
2025-11-19 21:15:13 +0800

    Update request.py

--- request.py
+++ request.py
@@ -1,48 +1,72 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from datetime import datetime, timedelta
 import os
+import json
 import time
 import pyautogui
 import keyboard
+
+filename = "savepos.json"
+
+if not os.path.exists(filename):
+    with open(filename, "w") as f:
+        json.dump({
+            "saved_position_Group": None,
+            "saved_position_Share": None,
+            "saved_position_Post": None,
+            "saved_position_RateLimited": None
+        }, f, indent=4)
+
+with open(filename, "r") as f:
+    savepos = json.load(f)
 
 Post_To_Share = "https://www.facebook.com/share/p/16bQW5zGHL/?mibextid=wwXIfr"
 Amount_Of_Groups = 45
 
 driver = webdriver.Chrome()
 
-saved_position_Group = None
-saved_position_Share = None
-saved_position_Post = None
 
 def clickGroup():
-    pyautogui.moveTo(saved_position_Group[0], saved_position_Group[1], duration=0.5)
+    pyautogui.moveTo(savepos["saved_position_Group"][0], savepos["saved_position_Group"][1], duration=0.5)
     time.sleep(2)
     pyautogui.click()
+
 
 def clickShare():
-    pyautogui.moveTo(saved_position_Share[0], saved_position_Share[1], duration=0.5)
+    pyautogui.moveTo(savepos["saved_position_Share"][0], savepos["saved_position_Share"][1], duration=0.5)
     time.sleep(2)
     pyautogui.click()
 
+
 def clickPost():
-    pyautogui.moveTo(saved_position_Post[0], saved_position_Post[1], duration=0.5)
+    pyautogui.moveTo(savepos["saved_position_Post"][0], savepos["saved_position_Post"][1], duration=0.5)
     time.sleep(2)
     pyautogui.click()
+
 
 def clsProgress(a):
     os.system('cls')
-    print(f"Progress: {a}/{Amount_Of_Groups} Groups.")
+    print(f"[PROGRESS] Progress: {a - 1}/{Amount_Of_Groups} Groups.")
+    print(f"[ATTEMPT] Attempting: Group {a} rightnow.")
+    print("\n")
+
+
+def clickLimitPost():
+    pyautogui.moveTo(savepos["saved_position_RateLimited"][0], savepos["saved_position_RateLimited"][1], duration=0.5)
+    time.sleep(2)
+    pyautogui.click()
+
 
 def main():
-    global saved_position_Share
-    global saved_position_Group
-    global saved_position_Post
+    global savepos
 
-    print("Navigating...")
+    print("[SYSTEM] Navigating...")
 
     for amount in range(1, Amount_Of_Groups + 1):
-        print(f"{amount} Groups")
+        Retry_Attempt = -1
+        clsProgress(amount)
 
         driver.get("https://facebook.com")
         time.sleep(3)
@@ -50,37 +74,41 @@ def main():
         driver.get(Post_To_Share)
         time.sleep(5)
 
-        if saved_position_Share is None:
-            print("Move your cursor to the 'Share' Button and press 'X' to complete setup")
+        if savepos["saved_position_Share"] is None:
+            print("[SYSTEM] Move your cursor to the 'Share' Button and press 'X' to complete setup")
 
-        while saved_position_Share is None:
+        while savepos["saved_position_Share"] is None:
             if keyboard.is_pressed('x'):
-                saved_position_Share = pyautogui.position()
+                savepos["saved_position_Share"] = list(pyautogui.position())
                 pyautogui.hotkey('alt', 'tab')
-                confirm_input = input("Confirm Position? Press [Y/N]: ")
+                confirm_input = input("[INPUT] Confirm Position? Press [Y/N]: ")
                 if confirm_input.upper() != "Y":
-                    saved_position_Share = None
+                    savepos["saved_position_Share"] = None
+                else:
+                    with open(filename, "w") as f:
+                        json.dump(savepos, f, indent=4)
 
         clickShare()
         time.sleep(5)
         clsProgress(amount)
 
-        if saved_position_Group is None:
-            print("Move your cursor to the 'Group' Button and press 'X' to complete setup")
+        if savepos["saved_position_Group"] is None:
+            print("[SYSTEM] Move your cursor to the 'Group' Button and press 'X' to complete setup")
 
-        while saved_position_Group is None:
+        while savepos["saved_position_Group"] is None:
             if keyboard.is_pressed('x'):
-                saved_position_Group = pyautogui.position()
+                savepos["saved_position_Group"] = list(pyautogui.position())
                 pyautogui.hotkey('alt', 'tab')
-                confirm_input = input("Confirm Position? Press [Y/N] ")
+                confirm_input = input("[INPUT] Confirm Position? Press [Y/N]: ")
                 pyautogui.hotkey('alt', 'tab')
 
-                if confirm_input.upper() == "Y":
-                    print(f"Position saved at: {saved_position_Group}")
-                    time.sleep(1)
+                if confirm_input.upper() != "Y":
+                    savepos["saved_position_Group"] = None
                 else:
-                    saved_position_Group = None
+                    with open(filename, "w") as f:
+                        json.dump(savepos, f, indent=4)
 
+        clsProgress(amount)
         clickGroup()
         time.sleep(5)
 
@@ -98,28 +126,59 @@ def main():
         pyautogui.keyDown('enter')
         time.sleep(10)
 
-        if saved_position_Post is None:
-            print("Move your cursor to the 'Post' Button and press 'X' to complete setup")
+        if savepos["saved_position_Post"] is None:
+            print("[SYSTEM] Move your cursor to the 'Post' Button and press 'X' to complete setup")
 
-        while saved_position_Post is None:
+        while savepos["saved_position_Post"] is None:
             if keyboard.is_pressed('x'):
-                saved_position_Post = pyautogui.position()
+                savepos["saved_position_Post"] = list(pyautogui.position())
                 pyautogui.hotkey('alt', 'tab')
-                confirm_input = input("Confirm Position? Press [Y/N]: ")
+                confirm_input = input("[INPUT] Confirm Position? Press [Y/N]: ")
                 if confirm_input.upper() != "Y":
-                    saved_position_Post = None
+                    savepos["saved_position_Post"] = None
+                else:
+                    with open(filename, "w") as f:
+                        json.dump(savepos, f, indent=4)
 
         clickPost()
+        time.sleep(5)
 
-        time.sleep(1)
-        pyautogui.keyDown('enter')
-        print("Shared Successfully!")
+        while True:
+            try:
+                element = driver.find_element(By.XPATH, "//div[contains(text(), 'We limit how often you can post')]")
+                Retry_Attempt = Retry_Attempt + 1
+                if savepos["saved_position_RateLimited"] is None:
+                    print("[SYSTEM] Ratelimited Post Position NOT DETECTED!")
+                    print("[SYSTEM] Move your cursor to the 'Post' Button and press 'X' to complete setup")
+                    while savepos["saved_position_RateLimited"] is None:
+                        if keyboard.is_pressed('x'):
+                            savepos["saved_position_RateLimited"] = list(pyautogui.position())
+                            pyautogui.hotkey('alt', 'tab')
+                            confirm_input = input("[INPUT] Confirm Position? Press [Y/N]: ")
+                            if confirm_input.upper() != "Y":
+                                savepos["saved_position_RateLimited"] = None
+                            else:
+                                with open(filename, "w") as f:
+                                    json.dump(savepos, f, indent=4)
+                clsProgress(amount)
+                now = datetime.now()
+                future_time = now + timedelta(minutes=10)
+                print(f"[SYSTEM] Retry Attempt: {Retry_Attempt}")
+                print("[TASK] RateLimit Detected!, Retrying in 10 minutes @ ", future_time.strftime("%H:%M:%S"))
+                time.sleep(600)
+                clickLimitPost()
+
+            except NoSuchElementException:
+                print("[TASK] Shared Successfully!")
+                break
+
         time.sleep(10)
+
 
 os.system('cls')
 driver.get("https://www.facebook.com")
 
-input("Press enter after logging in.")
+input("[INPUT] Press enter after logging in.")
 pyautogui.hotkey('alt', 'tab')
 
 os.system('cls')
